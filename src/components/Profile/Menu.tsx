931e9028aa02f151be4e65bc1b3fab67594f5f9d
Nicolas <nicolas.barrat@student.junia.com>
2022-12-14 14:40:54 +0100

    Correction Menu Profil

--- src/components/Profile/Menu.tsx
+++ src/components/Profile/Menu.tsx
@@ -1,113 +1,43 @@
-import { IonContent, useIonViewDidEnter, useIonViewWillEnter, useIonViewWillLeave, IonList, IonItem, IonLabel } from '@ionic/react';
-import { useHistory } from 'react-router';
+import {
+    IonContent,
+    useIonViewDidEnter,
+    useIonViewWillEnter,
+    useIonViewWillLeave,
+    IonList,
+    IonItem,
+    IonLabel
+} from '@ionic/react';
+import {useHistory} from 'react-router';
 import './Menu.css'
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Menu: React.FC = () => {
-    let [nomClasse, setNomClasse] = useState(0);
-
-    useIonViewDidEnter(() => {
-        console.log(history.location.pathname)
-        let Label;
-        switch(history.location.pathname) {
-            case '/factures':
-                Label = document.getElementById('2');
-                Label?.classList.add('selectBtn', 'btnMenu');
-                break;
-            case '/services':
-                Label = document.getElementById('3');
-                Label?.classList.add('selectBtn', 'btnMenu');
-                break;
-            case '/commandes':
-                Label = document.getElementById('4');
-                Label?.classList.add('selectBtn', 'btnMenu');
-                break;
-            case '/profile':
-                Label = document.getElementById('1');
-                Label?.classList.add('selectBtn', 'btnMenu');
-                break;
-            default:
-                break;
-        }
-    });
-
-    useIonViewWillLeave(() => {
-        setNomClasse(2);
-        console.log("sortie");
-        console.log((nomClasse === 1) ? 'egal à 1' : 'autre' + nomClasse);
-    });
-    // useIonViewWillEnter(() => {
-    //     console.log("enter");
-    //     switch (history.location.pathname) {
-    //         case '/profile/factures':
-    //             setNomClasse(2);
-    //             console.log(nomClasse)
-    //             break;
-    //         case '/profile/services':
-    //             setNomClasse(3);
-    //             console.log(nomClasse)
-    //             break;
-    //         case '/profile/commandes':
-    //             setNomClasse(4);
-    //             console.log(nomClasse)
-    //             break;
-    //         case '/profile':
-    //             setNomClasse(1);
-    //             console.log(nomClasse)
-    //             break;
-            
-    //         default:
-    //             console.log("default")
-    //             break;
-    //     }
-    // });
+    function getClassName(id: string) {
+        return history.location.pathname === '/profile' && id === '1' ?  'btnMenu selectBtn' :
+            history.location.pathname === '/factures' && id === '2' ?  'btnMenu selectBtn' :
+                history.location.pathname === '/services' && id === '3' ?  'btnMenu selectBtn' :
+                    history.location.pathname === '/commandes' && id === '4' ?  'btnMenu selectBtn' : 'btnMenu';
+    }
 
     const history = useHistory();
-
-    const routeChange = (path: string) => {
-        history.push(path);
-    }
-
-    function setbackground() {
-        console.log(history.location.pathname)
-        console.log((nomClasse === 1) ? 'selectBtn' : '')
-        switch (history.location.pathname) {
-            case '/profile/factures':
-                setNomClasse(2);
-                console.log(nomClasse)
-                break;
-            case '/profile/services':
-                setNomClasse(3);
-                console.log(nomClasse)
-                break;
-            case '/profile/commandes':
-                setNomClasse(4);
-                console.log(nomClasse)
-                break;
-            case '/profile':
-                setNomClasse(1);
-                console.log(nomClasse)
-                break;
-            
-            default:
-                console.log("default")
-                break;
-        }
-    }
 
     return (
         <div className='menu'>
             <IonList>
-                <IonItem id='1' routerLink="/profile" className={'btnMenu '}>
+                <IonItem id='1' routerLink="/profile"
+                         className={getClassName('1')}>
                     <IonLabel>Mon compte</IonLabel>
                 </IonItem>
-                <IonItem id='2' routerLink="/factures" className={'btnMenu '}>
+                <IonItem id='2' routerLink="/factures"
+                         className={getClassName('2')}>
                     <IonLabel>Mes factures</IonLabel>
                 </IonItem>
-                <IonItem id='3' routerLink="/services" className={'btnMenu '}>
+                <IonItem id='3' routerLink="/services"
+                         className={getClassName('3')}>
                     <IonLabel>Mes services</IonLabel>
                 </IonItem>
-                <IonItem routerLink="/commandes" className={'btnMenu '}>
+                <IonItem routerLink="/commandes"
+                         className={getClassName('4')}>
                     <IonLabel>Mes commandes</IonLabel>
                 </IonItem>
             </IonList>
@@ -125,7 +55,7 @@ const Menu: React.FC = () => {
             <div className={'btnMenu ' + (nomClasse == 4) ? 'selectBtn' : ''} onClick={e => (routeChange('/profile/commandes'))}>
                 <span className='name'>Mes commandes</span>
             </div> */}
-            <IonContent onLoad={setbackground}></IonContent>
+            {/*<IonContent onLoad={setbackground}></IonContent>*/}
         </div>
     );
 };
